c47a548d0d9196aaf1cad89a9b659afefdf73676
Сергей Салык <sergey.salyk@fabit.ru>
2023-06-11 18:59:48 +0300

    Generator data RU, ver 0.9

---
+++ app.py
@@ -0,0 +1,93 @@
+import tkinter as tk
+import webbrowser
+from generators import *
+from tkinter import messagebox
+
+class App:
+    def __init__(self, master):
+        self.master = master
+        master.title("Генератор данных")
+        master.geometry()
+
+        self.fields = [
+            ("ОКПО (Юр. лицо)", generate_okpo_for_juridical),
+            ("ОКПО (Филиал)", generate_okpo_for_branch),
+            ("ОГРН", generate_businesses_ogrn),
+            ("ИНН", generate_businesses_inn),
+            ("БИК", generate_businesses_bic),
+            ("Серия паспорта", generate_passport_series),
+            ("Номер", generate_number),
+            ("ДР взрослый", generate_date_adult),
+            ("Серия СоР", generate_sor_number),
+            ("Номер", generate_number),
+            ("ДР ребенок", generate_date_child),
+            ("ДР подросток", generate_date_child_adult),
+            ("Город", generate_name_city),
+            ("KPP", generate_kpp),
+            ("Телефон", generate_phone),
+        ]
+
+        self.result_texts = []
+
+        for field_name, field_generator in self.fields:
+            frame = tk.Frame(master)
+            frame.pack(fill=tk.X)
+
+            label = tk.Label(frame, text=field_name, width=20)
+            label.pack(side=tk.LEFT)
+
+            result_text = tk.Text(frame, height=1, width=30)
+            result_text.pack(side=tk.LEFT)
+            self.result_texts.append(result_text)
+
+            generate_button = tk.Button(frame, text="g", command=lambda generator=field_generator, text=result_text: self.generate_field(generator, text))
+            generate_button.pack(side=tk.LEFT)
+
+            copy_button = tk.Button(frame, text="Копировать", command=lambda text=result_text: self.copy_to_clipboard(text))
+            copy_button.pack(side=tk.LEFT)
+
+        author_label = tk.Label(master, text="Автор: Sergey Salyk", fg="blue", cursor="hand2")
+        author_label.pack()
+        author_label.bind("<Button-1>", lambda e: self.open_linkedin_profile())
+
+        self.pinned = False
+
+        self.pin_button = tk.Button(master, text="Закрепить", command=self.toggle_pinning)
+        self.pin_button.pack(side=tk.RIGHT)
+
+        generate_button = tk.Button(master, text="Сгенерировать все", command=self.generate_fields)
+        generate_button.pack()
+
+    def generate_field(self, field_generator, result_text):
+        result_text.delete(1.0, tk.END)
+        result_text.insert(tk.END, field_generator())
+
+    def generate_fields(self):
+        for i, (_, field_generator) in enumerate(self.fields):
+            result_text = self.result_texts[i]
+            result_text.delete(1.0, tk.END)
+            result_text.insert(tk.END, field_generator())
+
+    def copy_to_clipboard(self, text_widget):
+        text = text_widget.get(1.0, tk.END).strip()
+        if text:
+            self.master.clipboard_clear()
+            self.master.clipboard_append(text)
+
+    def open_linkedin_profile(self):
+        webbrowser.open("https://www.linkedin.com/in/sergeysalyk")
+
+    def toggle_pinning(self):
+        self.pinned = not self.pinned
+        if self.pinned:
+            self.pin_button.config(text="Открепить")
+            self.master.wm_attributes("-topmost", 1)
+        else:
+            self.pin_button.config(text="Закрепить")
+            self.master.wm_attributes("-topmost", 0)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop()
---
+++ generators.py
@@ -0,0 +1,119 @@
+from faker import Faker
+from random import randrange, randint
+import random
+from datetime import date, timedelta
+
+fake = Faker(['ru_RU'])
+Faker.seed(randint(2, 1000))
+
+
+def generate_okpo_for_juridical():
+    while True:
+        number_random = randrange(4444444, 5555555)
+        number = str(number_random)
+        if number and len(number) == 7:
+            break
+    s1 = sum((pos + 1) * int(num) for pos, num in enumerate(number))
+    s2 = 0
+    for pos, num in enumerate(number):
+        s2 += (pos + 1) * int(num)
+    checkdigit = s1 % 11
+    okpo = str(number) + str(checkdigit)
+    return okpo
+
+
+def generate_okpo_for_branch():
+    while True:
+        number_random = randrange(111111111, 999999999)
+        number = str(number_random)
+        if number and len(number) == 9:
+            s1 = sum((pos + 1) * int(num) for pos, num in enumerate(number))
+            s2 = 0
+            for pos, num in enumerate(number):
+                s2 += (pos + 1) * int(num)
+            checkdigit = s1 % 11
+            okpo_branch = str(number) + str(checkdigit) + '312333'
+            return okpo_branch
+
+
+def generate_passport_series():
+    digits = str(randint(1000, 9999))
+    return f"{digits}"
+
+
+def generate_number():
+    digits = str(randint(100000, 999999))
+    return f"{digits}"
+
+
+def generate_sor_number():
+    roman_numeral = random.choice(['II', 'IV', 'VI', 'IX', 'XI'])
+    russian_letter = ''.join([chr(random.randint(1040, 1071)) for i in range(2)])
+    return f"{roman_numeral}-{russian_letter}"
+
+
+def generate_name_city():
+    return fake.city_name()
+
+
+def generate_full_name_org():
+    return fake.city_name()
+
+
+def generate_kpp():
+    return fake.kpp()
+
+
+def generate_phone():
+    return fake.phone_number()
+
+
+def generate_pasport_number():
+    return fake.plate_number_extra()
+
+
+def generate_businesses_ogrn():
+    return fake.businesses_ogrn()
+
+
+def generate_businesses_inn():
+    return fake.businesses_inn()
+
+
+def generate_businesses_bic():
+    return fake.bic()
+
+
+def generate_businesses_inn_str():
+    businesses_inn = fake.businesses_inn()
+    return str(businesses_inn)
+
+
+def generate_date_child():
+    current_date = date.today()
+    min_age = 1
+    max_age = 8
+    min_birthdate = current_date - timedelta(days=max_age*365)
+    max_birthdate = current_date - timedelta(days=min_age*365)
+    birthdate = current_date - timedelta(days=randint(min_age*365, max_age*365))
+    return birthdate.strftime("%d.%m.%Y")
+
+
+def generate_date_child_adult():
+    current_date = date.today()
+    min_age = 14
+    max_age = 18
+    min_birthdate = current_date - timedelta(days=max_age*365)
+    max_birthdate = current_date - timedelta(days=min_age*365)
+    birthdate = current_date - timedelta(days=randint(min_age*365, max_age*365))
+    return birthdate.strftime("%d.%m.%Y")
+
+
+def generate_date_adult():
+    current_date = date.today()
+    min_age = 18
+    max_age = 40
+    min_birthdate = current_date - timedelta(days=max_age*365)
+    max_birthdate = current_date - timedelta(days=min_age*365)
+    birthdate = current_date - timedelta(days=randint(min_age*365, max_age*365))
+    return birthdate.strftime("%d.%m.%Y")
